c739812924816ec5034cf156c700f1713da77579
FranPisarri <franciscopisarri@gmail.com>
2025-07-06 23:23:37 -0300

    Pasaje entre escenas

--- Assets/UnitLoaderController.cs
+++ Assets/UnitLoaderController.cs
@@ -7,6 +7,7 @@ public class UnitLoaderController : MonoBehaviourPunCallbacks
 {
     public static UnitLoaderController Instance;
 
+    public bool[] players_ready = new bool[5];
     private void Awake()
     {
         if (Instance == null)
@@ -28,11 +29,24 @@ public class UnitLoaderController : MonoBehaviourPunCallbacks
     public void AddHeroe(UnitData heroe, int client_id)
     {
         playable_heroes[client_id] = heroe;
+        players_ready[client_id] = true;
+        CheckIfStart();
     }
 
     public void AddMapDM(Playable_Map new_playable_Map)
     {
         playable_Map = new_playable_Map;
+        players_ready[0] = true;
+        CheckIfStart();
+    }
+
+    void CheckIfStart()
+    {
+        foreach (bool ready in players_ready)
+        {
+            if (!ready) return;
+        }
+        TimerAndLoadGame.instance.LoadGame();
     }
 }
 
