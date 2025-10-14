00f54f400f841af94d487e4221fbc6a2981772ec
FranPisarri <franciscopisarri@gmail.com>
2025-10-14 15:49:38 -0300

    Mapa de fondo

--- Assets/Scripts/ActiveMap.cs
+++ Assets/Scripts/ActiveMap.cs
@@ -3,6 +3,23 @@ using UnityEngine;
 public class ActiveMap : MonoBehaviour
 {
     [SerializeField] GameObject[] map_list;
+    public bool is_game;
+
+    private void Start()
+    {
+        if (is_game)
+        {
+            var unit_l = UnitLoaderController.Instance;
+            if (unit_l != null)
+            {
+                ActivateMap(unit_l.playable_Map.Actual_map);
+            }
+            else
+            {
+                ActivateMap(Maps.MAP_NAME1);
+            }
+        }
+    }
 
     public void ActivateMap(Maps map)
     {
